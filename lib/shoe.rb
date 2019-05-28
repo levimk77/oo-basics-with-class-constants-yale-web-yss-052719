53b2163476aa0555677fc8a18c69c41b5effa536
Elijah Florence <efloren@siue.edu>
2019-05-28 17:40:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,13 @@
 class Shoe
+  BRANDS = []
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
