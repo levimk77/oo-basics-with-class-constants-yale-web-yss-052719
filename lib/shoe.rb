4b25ed306f5d31fd5eddf5c7bb400d37e07d88e4
Elijah Florence <efloren@siue.edu>
2019-05-28 17:46:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,10 +7,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
+    if BRANDS.include?('brand')
     BRANDS << brand
-  end
-  
+    end
   end
 
   def cobble
